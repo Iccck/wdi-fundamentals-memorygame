2855aaca4be4355c9d3cae03f57dffea7a330ee7
Ick Hyun Kwon <kwonih1020@Icks-MacBook-Pro.local>
2017-03-11 17:17:32 -0500

    created final functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,11 +9,43 @@
 //	alert('Sorry, try again!');
 // }
 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
 var board = document.getElementById('game-board');
 function createBoard() {
 	for (var i=0; i<cards.length; i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
 		board.appendChild(cardElement);
+		board.appendChild(cardElement);
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 	}
 }
+
+function isTwoCards () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+	if (cardsInPlay.length === 2) {
+		isMath(cardsInPlay);
+		cardsInPlay =[];
+	}
+}
+
+
+
+function isMath(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+createBoard ();
